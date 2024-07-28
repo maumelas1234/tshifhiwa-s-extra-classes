74c77997d3561184a43cd5197d235b1729f2ddf2
khetzo <khetzo50@gmail.com>
2024-07-28 22:14:08 +0200

    login back end added

--- Authentication/authentication.js
+++ Authentication/authentication.js
@@ -2,13 +2,39 @@
 
 
 
-    function getUserInput (){
+    function getUserInput (name,email,password){
 
-        let UserSignUpname = document.getElementById("UserSignUpname").value;
-        console.log(UserSignUpname);
-            let UserSignupname= document.getElementById("UserSignupname").value;
-            let UserSignPassword= document.getElementById("UserSignPassword").value;
-        
+       name= document.getElementById("UserSignUpname").value;
+           email= document.getElementById("userSignupEmail").value;
+           password= document.getElementById("password").value;
+            const UserInforArry= [{
+            name: name,
+            email:email,
+            password:password
+        }]
+if (email && email.lengh >10 && name && password  &&  email.substring(email.length - 10)=="@gmail.com" ){
+    if(!email.includes("@gmail.com") || !email.includes("@outlook.com")){
+ 
+    let username = email.replace("@gmail.com",'')
+            
+    alert('data cepterd')
+
+    }else {
+
+        alert('invalid email or password')
+    }
+   
+
+
+}else {
+
+    alert("missing vales pless fiil in the missing text box")
+}
+
+
+console.log(UserInforArry);
+console.log("khetzo wordla");
+
          return UserSignUpname;  
         }
     
